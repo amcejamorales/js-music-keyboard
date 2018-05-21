f7c012b75dc42eb85f4882372cb39a2e14e5f4dc
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-05-21 16:07:50 -0700

    Play audio using keys; use jQuery object to find correct audio files; stop any audio file playing as soon as another is played

--- noise.js
+++ noise.js
@@ -1,11 +1,25 @@
 $(document).ready( function() {
+  const validNotes = ['a', 'b', 'c', 'd', 'e', 'f', 'g'];
+
+  let playAudio = function playAudio(note) {
+    $('audio').each(function() {
+      this.pause();
+      this.currentTime = 0;
+    });
+    let noteId = `#${note}Audio`;
+    let audio = $(noteId)[0];
+    audio.play();
+  }
+
   $('.instrument').on('click', 'button', function() {
     let note = $(this)[0].innerHTML;
-    console.log(note);
-    let noteId = `${note}Audio`;
-    console.log(noteId);
-    let audio = document.getElementById(noteId);
-    console.log(audio);
-    audio.play();
+    playAudio(note);
+  });
+
+  $('body').keydown((event) => {
+    let note = event.key;
+    if (validNotes.includes(note)) {
+      playAudio(note);
+    }
   });
 });
