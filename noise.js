bb016eb8bb78b51903c30143b49e5d94aa94c5a2
Angelica Maria Ceja Morales <amcejamorales@gmail.com>
2018-05-21 15:45:02 -0700

    Play notes when user clicks on buttons

--- noise.js
+++ noise.js
@@ -1,3 +1,11 @@
 $(document).ready( function() {
-  // your code here
+  $('.instrument').on('click', 'button', function() {
+    let note = $(this)[0].innerHTML;
+    console.log(note);
+    let noteId = `${note}Audio`;
+    console.log(noteId);
+    let audio = document.getElementById(noteId);
+    console.log(audio);
+    audio.play();
+  });
 });
